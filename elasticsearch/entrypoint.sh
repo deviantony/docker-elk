6a602af12231af60184e3b8c165b34b8209a566e
Slava Shpitalny <slavik57@gmail.com>
2017-09-06 13:37:42 +0300

    Disable X-PACK
    License ended :(

--- elasticsearch/entrypoint.sh
+++ elasticsearch/entrypoint.sh
@@ -15,8 +15,9 @@ wget --tries=0 \
      --retry-connrefused \
      --read-timeout 5 \
      --wait 1 \
-      --user elastic --password $ELASTIC_USER_PASSWORD \
      'http://localhost:9200/'
+      # Uncommend this when we buy XPACK
+      # --user elastic --password $ELASTIC_USER_PASSWORD \
 
 # Check if elastic started
 WGET_RESULT=$?
@@ -25,53 +26,65 @@ if [ $WGET_RESULT -eq 4 ]; then
   exit 1
 fi;
 
+# Uncommend this when we buy XPACK
 # If unauthorized - means the elastic password was not set yet
-if [ $WGET_RESULT -eq 6 ]; then
-  echo "Setting elastic password"
+# if [ $WGET_RESULT -eq 6 ]; then
+#   echo "Setting elastic password"
   
-  curl -XPUT "http://localhost:9200/_xpack/security/user/elastic/_password?pretty" -H 'Content-Type: application/json' -d"
-  {
-    \"password\": \"$ELASTIC_USER_PASSWORD\"
-  }
-  " -u elastic:changeme
+#   curl -XPUT "http://localhost:9200/_xpack/security/user/elastic/_password?pretty" -H 'Content-Type: application/json' -d"
+#   {
+#     \"password\": \"$ELASTIC_USER_PASSWORD\"
+#   }
+#   " -u elastic:changeme
 
-fi;
+# fi;
 
+# Uncommend this when we buy XPACK
 # Set the password for the users: kibana, logstash_system
-curl -XPUT "http://localhost:9200/_xpack/security/user/kibana/_password?pretty" -H 'Content-Type: application/json' -d"
-{
-  \"password\": \"$KIBANA_USER_PASSWORD\"
-}
-"  -u elastic:$ELASTIC_USER_PASSWORD
+# curl -XPUT "http://localhost:9200/_xpack/security/user/kibana/_password?pretty" -H 'Content-Type: application/json' -d"
+# {
+#   \"password\": \"$KIBANA_USER_PASSWORD\"
+# }
+# " -u elastic:$ELASTIC_USER_PASSWORD
 
-curl -XPUT "http://localhost:9200/_xpack/security/user/logstash_system/_password?pretty" -H 'Content-Type: application/json' -d"
-{
-  \"password\": \"$LOGSTASH_SYSTEM_USER_PASSWORD\"
-}
-" -u elastic:$ELASTIC_USER_PASSWORD
+# curl -XPUT "http://localhost:9200/_xpack/security/user/logstash_system/_password?pretty" -H 'Content-Type: application/json' -d"
+# {
+#   \"password\": \"$LOGSTASH_SYSTEM_USER_PASSWORD\"
+# }
+# " -u elastic:$ELASTIC_USER_PASSWORD
 
 # Create indexes
 echo "Configuring indexes"
 
 curl -XPUT -D- "http://localhost:9200/.kibana/index-pattern/$ELASTIC_INDEX_PREFIX-*" \
     -H 'Content-Type: application/json' \
-    -d "{\"title\" : \"$ELASTIC_INDEX_PREFIX-*\", \"timeFieldName\": \"@timestamp\", \"notExpandable\": true}" -u elastic:$ELASTIC_USER_PASSWORD
+    -d "{\"title\" : \"$ELASTIC_INDEX_PREFIX-*\", \"timeFieldName\": \"@timestamp\", \"notExpandable\": true}" 
+    # Uncommend this when we buy XPACK
+    # -u elastic:$ELASTIC_USER_PASSWORD
 
 curl -XPUT -D- "http://localhost:9200/.kibana/index-pattern/$ELASTIC_INDEX_PREFIX-dev-*" \
     -H 'Content-Type: application/json' \
-    -d "{\"title\" : \"$ELASTIC_INDEX_PREFIX-dev-*\", \"timeFieldName\": \"@timestamp\", \"notExpandable\": true}" -u elastic:$ELASTIC_USER_PASSWORD
+    -d "{\"title\" : \"$ELASTIC_INDEX_PREFIX-dev-*\", \"timeFieldName\": \"@timestamp\", \"notExpandable\": true}" 
+    # Uncommend this when we buy XPACK
+    # -u elastic:$ELASTIC_USER_PASSWORD
 
 curl -XPUT -D- "http://localhost:9200/.kibana/index-pattern/$ELASTIC_INDEX_PREFIX-test-*" \
     -H 'Content-Type: application/json' \
-    -d "{\"title\" : \"$ELASTIC_INDEX_PREFIX-test-*\", \"timeFieldName\": \"@timestamp\", \"notExpandable\": true}" -u elastic:$ELASTIC_USER_PASSWORD
+    -d "{\"title\" : \"$ELASTIC_INDEX_PREFIX-test-*\", \"timeFieldName\": \"@timestamp\", \"notExpandable\": true}" 
+    # Uncommend this when we buy XPACK
+    # -u elastic:$ELASTIC_USER_PASSWORD
 
 curl -XPUT -D- "http://localhost:9200/.kibana/index-pattern/$ELASTIC_INDEX_PREFIX-prod-*" \
     -H 'Content-Type: application/json' \
-    -d "{\"title\" : \"$ELASTIC_INDEX_PREFIX-prod-*\", \"timeFieldName\": \"@timestamp\", \"notExpandable\": true}" -u elastic:$ELASTIC_USER_PASSWORD
+    -d "{\"title\" : \"$ELASTIC_INDEX_PREFIX-prod-*\", \"timeFieldName\": \"@timestamp\", \"notExpandable\": true}" 
+    # Uncommend this when we buy XPACK
+    # -u elastic:$ELASTIC_USER_PASSWORD
 
 # Set default index
 curl -XPUT -D- "http://localhost:9200/.kibana/config/5.5.0" \
     -H 'Content-Type: application/json' \
-    -d "{\"defaultIndex\": \"$ELASTIC_INDEX_PREFIX-*\"}" -u elastic:$ELASTIC_USER_PASSWORD
+    -d "{\"defaultIndex\": \"$ELASTIC_INDEX_PREFIX-*\"}" 
+    # Uncommend this when we buy XPACK
+    # -u elastic:$ELASTIC_USER_PASSWORD
 
 wait $ES_PID
