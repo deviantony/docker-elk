132637bc36ca807e78a591af2d5936939afc9dea
Piergiorgio Lucidi <piergiorgiolucidi@gmail.com>
2017-11-17 17:52:48 +0100

    Update version to 6.1.0 (#212)
    Includes changes from #196

--- elasticsearch/bin/init_sg.sh
+++ elasticsearch/bin/init_sg.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-plugins/search-guard-5/tools/sgadmin.sh -cd config/ -ts config/truststore.jks -ks config/kirk-keystore.jks -nhnv -icl
+plugins/search-guard-6/tools/sgadmin.sh \
+	-cd config/ \
+	-ts config/truststore.jks \
+	-ks config/kirk-keystore.jks \
+	-nhnv \
+	-icl
