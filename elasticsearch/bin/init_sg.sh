431502db04c8106f80b2b3bc8fb18391d3a1034d
Antoine Cotten <hello@acotten.com>
2019-08-02 17:05:23 +0200

    Update to 7.2.1
    Closes #421

--- elasticsearch/bin/init_sg.sh
+++ elasticsearch/bin/init_sg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-plugins/search-guard-6/tools/sgadmin.sh \
+plugins/search-guard-7/tools/sgadmin.sh \
 	-cd config/sg/ \
 	-ts config/sg/truststore.jks \
 	-ks config/sg/kirk-keystore.jks \
