1e1b2d35d591ea69e44a7cb2334a3e4ae445ab02
Antoine Cotten <hello@acotten.com>
2020-08-12 21:09:15 +0200

    Enable TLS in Elasticsearch
    Co-authored-by: wych42 <wych101010@gmail.com>

--- .github/workflows/scripts/disable-disk-alloc-decider.sh
+++ .github/workflows/scripts/disable-disk-alloc-decider.sh
@@ -10,15 +10,18 @@ source "${BASH_SOURCE[0]%/*}"/lib/testing.sh
 cid_es="$(container_id elasticsearch)"
 ip_es="$(service_ip elasticsearch)"
 
+es_ca_cert=$(realpath "$(dirname "${BASH_SOURCE[0]}")"/../../../tls/certs/ca/ca.crt)
+
 grouplog 'Wait for readiness of Elasticsearch'
-poll_ready "$cid_es" "http://${ip_es}:9200/" -u 'elastic:testpasswd'
+poll_ready "$cid_es" 'https://elasticsearch:9200/' --resolve "elasticsearch:9200:${ip_es}" --cacert "$es_ca_cert" -u 'elastic:testpasswd'
 endgroup
 
 log 'Disabling disk allocation decider'
 
 declare -a put_args=( '-X' 'PUT' '--fail-with-body' '-s' '-u' 'elastic:testpasswd'
 	'-H' 'Content-Type: application/json'
-	"http://${ip_es}:9200/_cluster/settings?pretty"
+	'https://elasticsearch:9200/_cluster/settings?pretty'
+	'--resolve' "elasticsearch:9200:${ip_es}" '--cacert' "$es_ca_cert"
 	'-d' '{"persistent":{"cluster.routing.allocation.disk.threshold_enabled":false}}'
 )
 declare response
