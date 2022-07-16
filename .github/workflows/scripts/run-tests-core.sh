b6eb747a4ad9b152338956357f8f1723958a30d3
Evan <binary-signal@users.noreply.github.com>
2022-07-17 00:00:38 +0200

    fix: Logstash TCP input uses IANA registered port 5000

--- .github/workflows/scripts/run-tests-core.sh
+++ .github/workflows/scripts/run-tests-core.sh
@@ -30,7 +30,7 @@ declare -i was_retried=0
 
 # retry for max 10s (5*2s)
 for _ in $(seq 1 5); do
-	if echo 'dockerelk' | nc -q0 "$ip_ls" 5000; then
+	if echo 'dockerelk' | nc -q0 "$ip_ls" 50000; then
 		break
 	fi
 
