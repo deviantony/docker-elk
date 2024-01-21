753285f2e7f41d616a7b3056331d0b819b572c19
Antoine Cotten <hello@acotten.com>
2024-01-21 22:48:12 +0100

    Update run-tests-filebeat.sh

--- .github/workflows/scripts/run-tests-filebeat.sh
+++ .github/workflows/scripts/run-tests-filebeat.sh
@@ -34,10 +34,10 @@ declare response
 declare -i count
 
 declare -i was_retried=0
-
+sleep 15
 # retry for max 60s (30*2s)
 for _ in $(seq 1 90); do
-	response="$(curl "http://${ip_es}:9200/filebeat-*/_search?q=*:*&pretty" -s -u elastic:testpasswd)"
+	response="$(curl "http://${ip_es}:9200/filebeat-*/_search?q=message:*&pretty" -s -u elastic:testpasswd)"
 
 	set +u  # prevent "unbound variable" if assigned value is not an integer
 	count="$(jq -rn --argjson data "${response}" '$data.hits.total.value')"
